17800cddbbd60edd641cb60703a3bc7e30692206
Dave Lu <daveylupie@gmail.com>
2022-09-19 23:36:18 -0400

    Add comments and remove unused code

--- Problem Set 1/prob4.py
+++ Problem Set 1/prob4.py
@@ -1,37 +1,14 @@
 import copy
 
-# the following three functions are taken from:
-# https://www.cs.cmu.edu/~112/notes/notes-2d-lists.html#printing
-def repr2dList(L):
-    if (L == []): return '[]'
-    output = [ ]
-    rows = len(L)
-    cols = max([len(L[row]) for row in range(rows)])
-    M = [['']*cols for row in range(rows)]
-    for row in range(rows):
-        for col in range(len(L[row])):
-            M[row][col] = repr(L[row][col])
-    colWidths = [0] * cols
-    for col in range(cols):
-        colWidths[col] = max([len(M[row][col]) for row in range(rows)])
-    output.append('[\n')
-    for row in range(rows):
-        output.append(' [ ')
-        for col in range(cols):
-            if (col > 0):
-                output.append(', ' if col < len(L[row]) else '  ')
-            output.append(M[row][col].rjust(colWidths[col]))
-        output.append((' ],' if row < rows-1 else ' ]') + '\n')
-    output.append(']')
-    return ''.join(output)
-
-def print2dList(L):
-    print(repr2dList(L))
+# the following function is taken from:
+# https://www.cs.cmu.edu/~112/notes/notes-strings.html#basicFileIO
 
 def read_file(path):
     with open(path, "rt") as f:
         return f.read()
 
+#performs DFS on a given graph.
+#returns True if we can find the target from the start
 def DFS(n, G, start, target):
     stack = [start]
     seen = []
@@ -44,25 +21,27 @@ def DFS(n, G, start, target):
                 if(i == target): return True
                 else: stack.insert(0, i)
 
-def remove_cycle_edges(n, graph_T, T, same_weight_edges):
+#removes any edge from edges that are in a cycle of the graph
+def remove_cycle_edges(n, graph_T, T, edges):
     remove = []
-    for edge in same_weight_edges:
+    for edge in edges:
         u, v = edge[1], edge[2]
         #remove the edge we're looking at from graph_T and then DFS from u to v
         temp_graph_T = copy.deepcopy(graph_T)
-        temp_graph_T[u][v] = 0
-        temp_graph_T[v][u] = 0
+        temp_graph_T[u][v] = 0      #removing the edge that we are looking at
+        temp_graph_T[v][u] = 0      #in order to use DFS to find a cycle
         if(DFS(n, temp_graph_T, u, v) == True):
+        #if we find that the edge is part of a cycle: mark for deletion
             remove.append((u, v))
-    for edge in remove:
+    for edge in remove: #delete marked edges
         T.remove(edge)
         u, v = edge
         graph_T[u][v] = 0
         graph_T[v][u] = 0
     return
 
-#edge format: [weight, start vertex, stop vertex]
-def intersect_mst_helper(graph_T, T, membership, edge):
+#adds an edge to the MST and updates the membership dictionary 
+def insert_edge(graph_T, T, membership, edge):
     u, v = edge[1], edge[2]
     if(membership[u] != membership[v]):
         membership[v] += membership[u]
@@ -72,44 +51,47 @@ def intersect_mst_helper(graph_T, T, membership, edge):
         graph_T[v][u] = 1
     return
 
+#primary function that finds the intersection MST
 def intersect_mst(n, G):
     T = []
     graph_T = [([0] * n) for i in range(n)]
     membership = dict() #how we will check for equal sets
-    for i in range(n):
+    for i in range(n):  #creation of the initial sets
         membership[i] = [i]
  
     edges = []
-    for i in range(n):
-        for j in range(i + 1, n): #looks at top half only, no need to do others
+    for i in range(n):      #finding all of the edges and saving them to sort 
+        for j in range(i + 1, n):   #looks at half only, no need for the others
             weight = G[i][j]
             if(weight != 0):
                 edges.append([weight, i, j])
     edges.sort()
 
-    same_weight_edges = []
+    same_weight_edges = [] #contains the edges we may need to delete later
     for i in range(len(edges)):
-        if(i != (len(edges) - 1) and edges[i][0] == edges[i + 1][0]): #weight of the next edge is the same as the previous
-            intersect_mst_helper(graph_T, T, membership, edges[i])
+        if(i != (len(edges) - 1) and edges[i][0] == edges[i + 1][0]):
+        #if weight of the next edge is the same as the previous
+            insert_edge(graph_T, T, membership, edges[i])
             same_weight_edges.append(edges[i])
         else:
-            intersect_mst_helper(graph_T, T, membership, edges[i])
+            insert_edge(graph_T, T, membership, edges[i])
             same_weight_edges.append(edges[i])
-            remove_cycle_edges(n, graph_T, T, same_weight_edges)
+            remove_cycle_edges(n, graph_T, T, same_weight_edges) #the intersect part
             same_weight_edges = []
     return T
 
+#takes as input, the name of the file as a string
+#files must be in the same format as the sample files
 def main(input_file):
     input = read_file(input_file).splitlines()
-    n = int(input[0])
-
-    for i in range(1, n + 1):
+    
+    n = int(input[0])                       
+    for i in range(1, n + 1):               
         input[i] = input[i].split("  ")
-    G = [([0] * n) for i in range(n)]
-
-    for i in range(1, n + 1):
+    G = [([0] * n) for i in range(n)]       #this section is just parsing
+    for i in range(1, n + 1):               #the file given
         for j in range(n):
             G[i - 1][j] = int(input[i][j])
+    
     return intersect_mst(n, G)
 
-print(main("sample2.txt"))
