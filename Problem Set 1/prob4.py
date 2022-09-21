6a0d3a709b7d9f4018f3cd2f7718620f814f9ba7
Dave Lu <daveylupie@gmail.com>
2022-09-21 16:04:08 -0400

    fixed dict checking bug
    bug with checking memberships was fixed
    
    new bug discovered: won't insert an edge in certain edge cases when you are supposed to

--- Problem Set 1/prob4.py
+++ Problem Set 1/prob4.py
@@ -7,10 +7,36 @@ The input file must be in the exact same format as the sample files.
 """
 # the following function is taken from:
 # https://www.cs.cmu.edu/~112/notes/notes-strings.html#basicFileIO
-
+# assists with opening files
 def read_file(path):
     with open(path, "rt") as f:
         return f.read()
+
+def repr2dList(L):
+    if (L == []): return '[]'
+    output = [ ]
+    rows = len(L)
+    cols = max([len(L[row]) for row in range(rows)])
+    M = [['']*cols for row in range(rows)]
+    for row in range(rows):
+        for col in range(len(L[row])):
+            M[row][col] = repr(L[row][col])
+    colWidths = [0] * cols
+    for col in range(cols):
+        colWidths[col] = max([len(M[row][col]) for row in range(rows)])
+    output.append('[\n')
+    for row in range(rows):
+        output.append(' [ ')
+        for col in range(cols):
+            if (col > 0):
+                output.append(', ' if col < len(L[row]) else '  ')
+            output.append(M[row][col].rjust(colWidths[col]))
+        output.append((' ],' if row < rows-1 else ' ]') + '\n')
+    output.append(']')
+    return ''.join(output)
+
+def print2dList(L):
+    print(repr2dList(L))
 
 #performs DFS on a given graph.
 #returns True if we can find the target from the start
@@ -51,7 +77,8 @@ def insert_edge(graph_T, T, membership, edge):
     u, v = edge[1], edge[2]
     if(membership[u] != membership[v]):
         membership[v] += membership[u]
-        membership[u] = membership[v]
+        for x in membership[v]:
+            membership[x] = membership[v]
         T.append((u, v))
         graph_T[u][v] = 1
         graph_T[v][u] = 1
@@ -64,7 +91,7 @@ def intersect_mst(n, G):
     membership = dict() #how we will check for equal sets
     for i in range(n):  #creation of the initial sets
         membership[i] = [i]
- 
+
     edges = []
     for i in range(n):      #finding all of the edges and saving them to sort 
         for j in range(i + 1, n):   #looks at half only, no need for the others
@@ -72,6 +99,7 @@ def intersect_mst(n, G):
             if(weight != 0):
                 edges.append([weight, i, j])
     edges.sort()
+    print("edges =", edges)
 
     same_weight_edges = [] #contains the edges we may need to delete later
     for i in range(len(edges)):
@@ -82,6 +110,7 @@ def intersect_mst(n, G):
         else:
             insert_edge(graph_T, T, membership, edges[i])
             same_weight_edges.append(edges[i])
+            print2dList(graph_T)
             remove_cycle_edges(n, graph_T, T, same_weight_edges) #the intersect part
             same_weight_edges = []
     return T
@@ -89,8 +118,8 @@ def intersect_mst(n, G):
 #takes as input, the name of the adjacency matrix file as a string
 #files must be in the same format as the sample files
 #returns the intersection MST as a list of edges
-def main():
-    input_file = sys.argv[-1]
+def main(input_file):
+    # input_file = sys.argv[-1]
     input = read_file(input_file).splitlines()
     
     n = int(input[0])                       
@@ -103,7 +132,7 @@ def main():
     
     MST = intersect_mst(n, G)
 
-    print(MST)
+    print("MST edges =", MST)
     return MST
 
-main()
+main("test2.txt")
