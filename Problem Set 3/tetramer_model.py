39ed273ee62628399f3ed40411647c19e99e48ed
Dave Lu <daveylupie@gmail.com>
2022-10-26 17:25:34 -0400

    fix noise coefficients to reflect correct

--- Problem Set 3/tetramer_model.py
+++ Problem Set 3/tetramer_model.py
@@ -33,14 +33,15 @@ def model(r, d, dt, dx, k1, k2, X, total_T):
         for i in range(1, points - 1):
             z1 = random.normalvariate(0, 1)
             z2 = random.normalvariate(0, 1)
-            A_part1 = A[i] + dt * d * ((A[i + 1] + A[i - 1] - 2*A[i]) / dx**2)
+            A_part1 = dt * d * ((A[i + 1] + A[i - 1] - 2*A[i]) / dx**2)
             A_part2 = 4 * dt * (k2 * B[i] - k1 * A[i]**4)
-            A_part3 = 2 * r * math.sqrt(dt) * (z2 * math.sqrt(k2 * B[i]) - z1 * math.sqrt(k1 * A[i]**4))
-            B_part1 = B[i] + dt * (k1 * A[i]**4 - k2 * B[i])
+            A_part3 = 4 * r * math.sqrt(dt) * (z2 * math.sqrt(k2 * B[i]) - z1 * math.sqrt(k1 * A[i]**4))
+            B_part1 = dt * (k1 * A[i]**4 - k2 * B[i])
             B_part2 = r * math.sqrt(dt) * (-z2 * math.sqrt(k2 * B[i]) + z1 * math.sqrt(k1 * A[i]**4))
+            
+            A[i] = A[i] + A_part1 + A_part2 + A_part3
+            B[i] = B[i] + B_part1 + B_part2
 
-            A[i] = A_part1 + A_part2 + A_part3
-            B[i] = B_part1 + B_part2
             if(A[1] < 0): A[i] = 0
             if(B[i] < 0): B[i] = 0
             
