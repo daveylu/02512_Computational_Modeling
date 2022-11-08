fcc8a50b275f9cddeb62411a082fd1d95b30c91e
Dave Lu <daveylupie@gmail.com>
2022-11-08 12:32:40 -0500

    changed algo to be correct and added test cases

--- Problem Set 4/zombies.py
+++ Problem Set 4/zombies.py
@@ -1,18 +1,13 @@
-import sys
-import math
+from numpy import random
 
-def read_file(path):
-    with open(path, "rt") as f:
-        return f.read()
-
-def passive_model(x1, x2, x3, S, I, D, N, T):
+def passive_model(x1, x2, S, I, D, N, T):
     all_infected = 0
     for i in range(T):
         t = 0
         while(I > 0):
-            t_i = math.exp((x1 * I * S) / N)
-            t_d = math.exp(x2 * I)
-            if(t_i < t_d):
+            t_i = random.exponential((x1 * I * S) / N)
+            t_d = random.exponential(x2 * I)
+            if(t_i < t_d and S > 0):
                 S -= 1
                 I += 1
                 t += t_i
@@ -30,10 +25,10 @@ def active_model(x1, x2, x3, S, I, D, N, T):
     for i in range(T):
         t = 0
         while(I > 0):
-            t_i = math.exp((x1 * I * S) / N)
-            t_d = math.exp(x2 * I)
-            t_k = math.exp((x3 * I * S) / N)
-            if(t_i < t_d and t_i < t_k):
+            t_i = random.exponential((x1 * I * S) / N)
+            t_d = random.exponential(x2 * I)
+            t_k = random.exponential((x3 * I * S) / N)
+            if(t_i < t_d and t_i < t_k and S > 0):
                 S -= 1
                 I += 1
                 t += t_i
@@ -44,17 +39,22 @@ def active_model(x1, x2, x3, S, I, D, N, T):
         if(S == 0): all_infected += 1
     return all_infected / T
 
-def main():
-    input_file = sys.argv[-1]
-    input = read_file(input_file).splitlines()
-    x1 = input[0]
-    x2 = input[1]
-    x3 = input[2]
-    N = input[3]
-    m = input[4]
-    T = input[5]
+def wrapper(x1, x2, x3, N, m, T):
     S = N - m
     I = N
     D = 0
-    
+    passive_result = passive_model(x1, x2, S, I, D, N, T)
+    active_result = active_model(x1, x2, x3, S, I, D, N, T)
+    print(f"x2 = {x2}")
+    print(f"Passive Model - fraction of all infected: {passive_result}")
+    print(f"Active Model - fraction of all infected: {active_result}")
 
+def main():
+    wrapper(1.5, 0.001, 1, 10000, 100, 100)
+    wrapper(1.5, 0.01, 1, 10000, 100, 100)
+    wrapper(1.5, 0.1, 1, 10000, 100, 100)
+    wrapper(1.5, 1, 1, 10000, 100, 100)
+    wrapper(1.5, 10, 1, 10000, 100, 100)
+    wrapper(1.5, 100, 1, 10000, 100, 100)
+
+main()
